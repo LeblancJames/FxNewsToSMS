245bb9fed124c2261d0c07968ec66792b86dde1e
LeblancJames <shij1245@gmail.com>
2023-10-06 04:37:02 -0400

    completed

--- sendsms.py
+++ sendsms.py
@@ -1,5 +1,38 @@
 from scrapper import scrape
+import os
+from dotenv import load_dotenv
+
+import smtplib
+from email.message import EmailMessage
+
+load_dotenv()
+user = os.environ.get("EMAIL")
+password = os.environ.get("PASS")
 
 
 def sendsms():
     scrapped_data = scrape()
+    filtered_data = scrapped_data[(scrapped_data['impact'] == 'High Impact Expected') & ((
+        scrapped_data['currency'] == 'USD') | (
+        scrapped_data['currency'] == 'EUR'))]
+    return filtered_data.to_string(index=False, header=False)
+
+
+def email_alert(subject, body, to):
+    msg = EmailMessage()
+    msg.set_content(body)
+    msg['subject'] = subject
+    msg['to'] = to
+    msg['from'] = "FxNewsToSMS"  # doesnt work??
+
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(user, password)
+    server.send_message(msg)
+    server.quit()
+
+
+if __name__ == '__main__':
+    data = sendsms()
+    email_alert("Good Morning! Here's todays news.", data,
+                "9179295977@mms.cricketwireless.net")
